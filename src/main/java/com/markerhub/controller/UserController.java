d8e7f3e52b852bdf6d4322a7d81f61ebce53b0f0
mning6229@gmail.com <ning.mo@dxc.com>
2021-09-28 22:09:12 +0800

    Initial commit

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.markerhub.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2021-09-05
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
---
+++ vueblog-master/vueblog-vue/readme.txt
@@ -0,0 +1,11 @@
+# 配置镜像加速
+https://www.ydyno.com/archives/1219.html
+
+# 安装依赖
+npm install
+
+# 启动服务 localhost:8013
+npm run dev
+
+# 构建生产环境
+npm run build:prod
